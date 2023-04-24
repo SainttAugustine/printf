952181b436239ef602d1ad157aab43223300805c
KennethTL <kennethtemba17@gmail.com>
2023-04-24 23:55:42 +0300

    Add header file

---
+++ cst_printf.h
@@ -0,0 +1,17 @@
+#ifndef CST_PRINTF_H
+#define CST_PRINTF_H
+
+#include <unistd.h>
+#include <stdarg.h>
+
+int cst_printf(const char *s, ...);
+int cst_putchar(char c);
+int cst_putstr(char *str);
+int cst_formats(const char *str, va_list ap, int i);
+int cst_putnbr(int n);
+int cst_print_percent(void);
+int cst_putnbr_u(unsigned long n);
+int cst_print_hex(unsigned long long n, char format);
+int cst_print_addr(unsigned long long n);
+
+#endif
