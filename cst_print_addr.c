f7df5cafb9fbf2564ca90b57f3f80c85595b1b72
KennethTL <kennethtemba17@gmail.com>
2023-04-25 09:16:11 +0300

    Add address printing capability

---
+++ cst_print_addr.c
@@ -0,0 +1,17 @@
+#include "cst_printf.h"
+
+/**
+* cst_print_addr - prints out an address specified by %p
+* @n: number of charcters to be printed
+* Return: length of the characters printed to stdout
+*/
+int cst_print_addr(unsigned long long n)
+{
+	int len;
+
+	len = 0;
+	len = len + write(1, "0x", 2);
+	len = len + cst_print_hex(n, 'x');
+
+	return (len);
+}
